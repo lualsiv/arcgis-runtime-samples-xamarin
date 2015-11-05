bf9bdde8d8ab96fd47764b7ce608c3a2027963be
Katy Dalton <kdalton@esri.com>
2015-11-05 08:46:31 -0700

    Initial commit of Xamarin Forms viewer. Works for iOS. Needs re-architecture to work for Android.

---
+++ src/Forms/Xamarin.Forms/Shared/Samples/MapView/ExampleSample/ExampleSample.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ArcGISRuntimeXamarin.Samples.ExampleSample
+{
+	public partial class ExampleSample : ContentPage
+	{
+		public ExampleSample ()
+		{
+			InitializeComponent ();
+
+            Title = "Test Example!";
+		}
+	}
+}
