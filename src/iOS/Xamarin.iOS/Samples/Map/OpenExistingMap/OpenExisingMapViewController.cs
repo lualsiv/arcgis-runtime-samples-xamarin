637c2bdd91db8fb922ca7d4929d8bf55ee41d706
unknown <apur7078@agoyalwin10.esri.com>
2015-11-10 14:02:10 -0800

    updating the name of the sample folder

--- src/iOS/Xamarin.iOS/Samples/Map/OpenExistingMap/OpenExisingMapViewController.cs
+++ src/iOS/Xamarin.iOS/Samples/Map/OpenExistingMap/OpenExisingMapViewController.cs
@@ -7,10 +7,10 @@ using Foundation;
 using Esri.ArcGISRuntime.Controls;
 using Esri.ArcGISRuntime;
 
-namespace ArcGISRuntimeXamarin.Samples.MapFromPortal
+namespace ArcGISRuntimeXamarin.Samples.OpenExistingMap
 {
     [Register("MapFromPortalViewController")]
-    public class MapFromPortalViewController : UIViewController
+    public class OpenExistingMapViewController : UIViewController
     {
         //String array to hold urls to publicly available web maps
         private string[] itemURLs = new string[] { "http://www.arcgis.com/home/item.html?id=2d6fa24b357d427f9c737774e7b0f977"
@@ -20,10 +20,10 @@ namespace ArcGISRuntimeXamarin.Samples.MapFromPortal
         //String array to store titles for the webmaps specified above. These titles are in the same order as the urls above
         private string[] titles = new string[] { "Housing with Mortgages", "USA Tapestry Segmentation", "Geology of United States" };
 
-        //Variable to hold map instance
+        //Variable to hold Map instance
         private Map myMap;
         
-        public MapFromPortalViewController()
+        public OpenExistingMapViewController()
         {
           
         }
@@ -37,16 +37,16 @@ namespace ArcGISRuntimeXamarin.Samples.MapFromPortal
             //Create a variable to hold the height of the button that we will be adding later on in the UI.
             var height = 45;
 
-            //Create a new mapview control and provide its location coordinates on the frame.
+            //Create a new MapView control and provide its location coordinates on the frame.
             MapView myMapView = new MapView()
             {
                 Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height- height)
             };
 
-            //Create a new map instance with url of the web map that is displayed by default
+            //Create a new Map instance with url of the web map that is displayed by default
             myMap = new Map(new Uri(itemURLs[0]));
 
-            //Assign this map to the mapview that was created above.
+            //Assign this map to the MapView that was created above.
             myMapView.Map = myMap;
 
             //Now we add a button at the bottom that will present users different webmaps that they can select to open.
@@ -61,28 +61,28 @@ namespace ArcGISRuntimeXamarin.Samples.MapFromPortal
 
             //Add event for button click. Clicking this button opens an action sheet with all the web maps in itemUrls above being presented as options. Users can select a webmap to open it.
             mapsButton.TouchUpInside += (s, e) => {
-                //Initialize an UIAlertController with a title and style of an actionsheet
+                //Initialize an UIAlertController with a title and style of an ActionSheet
                 UIAlertController actionSheetAlert = UIAlertController.Create("Select a map to open", "", UIAlertControllerStyle.ActionSheet);
-                //Add actions to action sheet. Selecting an option re-initializes the map with selected web map url and assign it to mapview
-                actionSheetAlert.AddAction(UIAlertAction.Create(titles[1], UIAlertActionStyle.Default, (action) =>
+                //Add actions to ActionSheet. Selecting an option re-initializes the Map with selected web map url and assign it to MapView
+                actionSheetAlert.AddAction(UIAlertAction.Create(titles[0], UIAlertActionStyle.Default, (action) =>
                   {
-                      myMap = new Map(new Uri(itemURLs[1]));
+                      myMap = new Map(new Uri(itemURLs[0]));
                       myMapView.Map = myMap;
                   }));
+                actionSheetAlert.AddAction(UIAlertAction.Create(titles[1], UIAlertActionStyle.Default, (action) =>
+                {
+                    myMap = new Map(new Uri(itemURLs[1]));
+                    myMapView.Map = myMap;
+                }));
                 actionSheetAlert.AddAction(UIAlertAction.Create(titles[2], UIAlertActionStyle.Default, (action) =>
                 {
                     myMap = new Map(new Uri(itemURLs[2]));
                     myMapView.Map = myMap;
                 }));
-                actionSheetAlert.AddAction(UIAlertAction.Create(titles[0], UIAlertActionStyle.Default, (action) =>
-                {
-                    myMap = new Map(new Uri(itemURLs[0]));
-                    myMapView.Map = myMap;
-                }));
                 this.PresentViewController(actionSheetAlert, true, null);
             };
 
-            //Finally add the mapview to the Subview
+            //Finally add the MapView to the Subview
             View.AddSubviews(myMapView, mapsButton);
 
         }
