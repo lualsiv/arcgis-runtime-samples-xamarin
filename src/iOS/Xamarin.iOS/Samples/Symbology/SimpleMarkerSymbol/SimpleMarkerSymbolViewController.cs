ff61a19bfd4829547042a6e2348678742a20929c
Katy Dalton <kdalton@esri.com>
2015-12-17 09:25:43 -0700

    Update to comments

--- src/iOS/Xamarin.iOS/Samples/Symbology/SimpleMarkerSymbol/SimpleMarkerSymbolViewController.cs
+++ src/iOS/Xamarin.iOS/Samples/Symbology/SimpleMarkerSymbol/SimpleMarkerSymbolViewController.cs
@@ -21,38 +21,38 @@ namespace ArcGISRuntimeXamarin.Samples.SimpleMarkerSymbol
         {
             base.ViewDidLoad();
 
-            //Create a new tiled layer and pass a Uri to the service
+            // Create a new tiled layer and pass a Uri to the service
             var baseLayer = new ArcGISTiledLayer(new Uri("http://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer"));
 
-            //Load the base layer 
+            // Load the base layer 
             await baseLayer.LoadAsync();
 
-            //Create a basemap and add the base layer
+            // Create a basemap and add the base layer
             var myBasemap = new Basemap();
             myBasemap.BaseLayers.Add(baseLayer);
 
-            //Create a new MapView control and provide its location coordinates on the frame
+            // Create a new MapView control and provide its location coordinates on the frame
             MapView myMapView = new MapView()
             {
                 Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height)
             };
 
-            //Create a new map instance and add the basemap
+            // Create a new map instance and add the basemap
             Map myMap = new Map(myBasemap);
 
-            //Assign this Map to the MapView
+            // Assign this Map to the MapView
             myMapView.Map = myMap;
 
-            //Create a new instance of GraphicsOverlay where we can create graphics with simple marker symbol 
+            // Create a new instance of GraphicsOverlay where we can create graphics with simple marker symbol 
             GraphicsOverlay graphicsOverlay = new GraphicsOverlay()
             {
                 RenderingMode = GraphicsRenderingMode.Static
             };
 
-            //Create a MapPoint where the Graphic will be located
+            // Create a MapPoint where the Graphic will be located
             var mapPoint = new MapPoint(-117.195646, 34.056397, SpatialReferences.Wgs84);
 
-            //Create a new red circle marker symbol
+            // Create a new red circle marker symbol
             var circleSymbol = new Esri.ArcGISRuntime.Symbology.SimpleMarkerSymbol()
             {
                 Style= SimpleMarkerSymbolStyle.Circle,
@@ -66,13 +66,13 @@ namespace ArcGISRuntimeXamarin.Samples.SimpleMarkerSymbol
             // Add the graphic to the graphicsOverlay
             graphicsOverlay.Graphics.Add(pointGraphic);
 
-            //Add the graphicsOverlay to MapView's GraphicsOverlays collection
+            // Add the graphicsOverlay to MapView's GraphicsOverlays collection
             myMapView.GraphicsOverlays.Add(graphicsOverlay);
 
             // Center the map view extent on the point graphic          
             myMapView.SetViewpoint(new Viewpoint(mapPoint, 24000));
 
-            //Add the MapView to the Subview
+            // Add the MapView to the Subview
             View.AddSubview(myMapView);
         }
     }
