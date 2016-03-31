13d7ed4925d5e740ce5a190873ee07855b38c41f
Antti Kajanus <antti.kajanus@live.com>
2016-03-31 15:30:48 +0100

    Added Set Min &  Max Scale sample

---
+++ src/Android/Xamarin.Android/Samples/Map/SetMinMaxScale/SetMinMaxScale.cs
@@ -0,0 +1,63 @@
+//Copyright 2015 Esri.
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Geometry;
+
+namespace ArcGISRuntimeXamarin.Samples.SetMinMaxScale
+{
+    [Activity(Label = "Set Min & Max Scale")]
+    public class SetMinMaxScale : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Title = "Set Min & Max Scale";
+
+            // Create new Map with Streets basemap 
+            var myMap = new Map(Basemap.CreateStreets());
+
+            // Set the scale at which this layer can be viewed
+            // MinScale defines how far 'out' you can zoom where
+            // MaxScale defines how far 'in' you can zoom.
+            myMap.MinScale = 8000;
+            myMap.MaxScale = 2000;
+
+            // Create a new map view control to display the map
+            var myMapView = new MapView();
+            myMapView.Map = myMap;
+
+            // Create central point where map is centered
+            var centralPoint = new MapPoint(-355453, 7548720, SpatialReferences.WebMercator);
+            // Create starting viewpoint
+            var startingViewpoint = new Viewpoint(
+                centralPoint,
+                3000);
+            // Set starting viewpoint
+            myMapView.SetViewpoint(startingViewpoint);
+
+            // Create main layout for the UI and add MapView into it
+            var layout = new LinearLayout(this) { Orientation = Orientation.Vertical };
+            layout.AddView(myMapView);
+
+            // Apply the layout to the page
+            SetContentView(layout);
+        }
+    }
+}
