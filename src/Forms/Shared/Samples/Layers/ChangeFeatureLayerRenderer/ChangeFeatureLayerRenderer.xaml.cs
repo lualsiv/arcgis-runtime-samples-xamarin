872fef0341f00646baf178bae2118bfdf77c4e8e
don1760 <dkemlage@esri.com>
2016-04-14 14:02:12 -0700

    Update the Xamarin Forms comments and Legal text to be uniform across all samples.

--- src/Forms/Shared/Samples/Layers/ChangeFeatureLayerRenderer/ChangeFeatureLayerRenderer.xaml.cs
+++ src/Forms/Shared/Samples/Layers/ChangeFeatureLayerRenderer/ChangeFeatureLayerRenderer.xaml.cs
@@ -1,16 +1,11 @@
-﻿// Copyright 2015 Esri.
+﻿// Copyright 2016 Esri.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
 
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
@@ -34,7 +29,9 @@ namespace ArcGISRuntimeXamarin.Samples.ChangeFeatureLayerRenderer
         public ChangeFeatureLayerRenderer ()
 		{
 			InitializeComponent ();
+
             Title = "Change feature layer renderer";
+
             //setup the control references and execute initialization 
             Initialize();
         }
@@ -65,8 +62,6 @@ namespace ArcGISRuntimeXamarin.Samples.ChangeFeatureLayerRenderer
             // Initialize a new feature layer based on the feature table
             _featureLayer = new FeatureLayer(featureTable);
 
-            // TODO: Remove this workaround
-            // Workaround for feature table initialization issue
             // Make sure that the feature layer gets loaded
             await _featureLayer.LoadAsync();
 
--- src/Forms/Shared/Samples/Map/ChangeBasemap/ChangeBasemap.xaml.cs
+++ src/Forms/Shared/Samples/Map/ChangeBasemap/ChangeBasemap.xaml.cs
@@ -1,16 +1,11 @@
-﻿// Copyright 2015 Esri.
+﻿// Copyright 2016 Esri.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
 
 using Esri.ArcGISRuntime.Mapping;
 using System;
@@ -55,21 +50,29 @@ namespace ArcGISRuntimeXamarin.Samples.ChangeBasemap
             switch (selectedBasemap)
             {
                 case "Topo":
+
                     // Set the basemap to Topographic
                     MyMapView.Map.Basemap = Basemap.CreateTopographic();
                     break;
+
                 case "Streets":
+
                     // Set the basemap to Streets
                     MyMapView.Map.Basemap = Basemap.CreateStreets();
                     break;
+
                 case "Imagery":
+
                     // Set the basemap to Imagery
                     MyMapView.Map.Basemap = Basemap.CreateImagery();
                     break;
+
                 case "Ocean":
+
                     // Set the basemap to Imagery
                     MyMapView.Map.Basemap = Basemap.CreateOceans();
                     break;
+
                 default:
                     break;
             }
