f3674538be3e68a0e8e911ad71e27b1419d585b3
Apurva Goyal <agoyal@esri.com>
2016-05-04 11:25:17 -0700

    adding enable location permission (#124)
    * adding enable location permission
    
    * adding location permission to android project

--- src/Android/Xamarin.Android/Resources/Resource.Designer.cs
+++ src/Android/Xamarin.Android/Resources/Resource.Designer.cs
@@ -48,10 +48,7 @@ namespace ArcGISRuntimeXamarin
 			public const int Android_512 = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int esri = 2130837505;
-			
-			// aapt resource value: 0x7f020002
-			public const int Icon = 2130837506;
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
