5d9bda1d31b00fa7e79027683da15786b69e6ddf
antt0000 <akajanus@esri.com>
2016-05-26 17:13:35 +0100

    Workaround for UWP navigation

--- src/Forms/UWP/MainPage.xaml.cs
+++ src/Forms/UWP/MainPage.xaml.cs
@@ -7,6 +7,8 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
 // language governing permissions and limitations under the License.
 
+using Windows.UI.Core;
+
 namespace ArcGISRuntimeXamarin.UWP
 {
     public sealed partial class MainPage
@@ -15,6 +17,26 @@ namespace ArcGISRuntimeXamarin.UWP
         {
             InitializeComponent();
             LoadApplication(new ArcGISRuntimeXamarin.App());
+
+            // Workaround : If ContentPage that is shown contains MapView, first backwards navigation removes the MapView
+            // and doesn't do the full navigation. As a workaround, check if there are 3 items in a navigation stack and 
+            // manually remove the sample page. 
+            // Items in the stack are following
+            // [0] : CategoriesPage
+            // [1] : SampleListPage
+            // [2] : Selected sample 
+            // [X] : Possible other pages
+            SystemNavigationManager.GetForCurrentView().BackRequested += ContentPageRenderer_BackRequested;
+        }
+
+        private void ContentPageRenderer_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            var x = Xamarin.Forms.Application.Current.MainPage.Navigation.NavigationStack;
+            // If we are on Samples page remove it from the stack.
+            if (x.Count == 2)
+            {
+                Xamarin.Forms.Application.Current.MainPage.Navigation.PopAsync();
+            }
         }
     }
 }
